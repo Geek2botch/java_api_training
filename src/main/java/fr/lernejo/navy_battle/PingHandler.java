042b53f8f1035333ee43fa56c0eaa5c9dc92ea82
quentin.nouguier <q.nouguier98@gmail.com>
2022-03-29 18:51:54 +0200

    updtae test files

--- src/main/java/fr/lernejo/navy_battle/PingHandler.java
+++ src/main/java/fr/lernejo/navy_battle/PingHandler.java
@@ -1,19 +1,14 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
-import com.sun.net.httpserver.HttpExchange;
-
-public class PingHandler implements HttpHandler {
+class PingHandler implements HttpHandler
+{
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        String body = "Hello";
-        exchange.sendResponseHeaders(200, body.length());
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(body.getBytes());
-        }
+    public void handle(HttpExchange t) throws IOException {
+        Utilities.sendResponse(t, 200, "OK");
     }
 }
--- src/test/java/fr/lernejo/navy_battle/UtilitiesTest.java
+++ src/test/java/fr/lernejo/navy_battle/UtilitiesTest.java
@@ -1,27 +1,30 @@
 package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpServer;
-import jdk.jshell.execution.Util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-class PingHandlerTest {
+class UtilitiesTest {
     @Test
-    public void pingTest() {
+    public void transAlphaTest() {
+        Assertions.assertEquals(Utilities.translatePosToAlpha(new BoardPosition(0,0)), "A1");
+    }
+
+    @Test
+    public void getWithHeaderTest() {
         try {
             GameState game = new GameState("http://localhost:9876");
             HttpServer server = Launcher.setupServer(game);
 
-            HttpResponse<String> response = Utilities.sendGetRequest("http://localhost:9876/ping");
+            HttpResponse<String> response = Utilities.sendGetRequestWithHeader("http://localhost:9876/ping");
+            Assertions.assertEquals(response.statusCode(), 200);
             Assertions.assertEquals(response.body(), "OK");
+
             server.stop(1);
         } catch (Exception e) {
-            System.out.println("Exception occurred during test : " + e);
+            System.out.println("Exception occurred during get test : " + e);
         }
     }
 }
